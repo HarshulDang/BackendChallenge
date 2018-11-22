11dc28d09198c0cd5c4d37b1fbf40767c66df8e8
HarshulDang <dangharshul199430@gmail.com>
2018-11-22 20:37:11 +0530

    Wrote PUT and DELETE functions

--- server.js
+++ server.js
@@ -14,3 +14,28 @@ app.get('/process', function (req, res) {
 		 res.json({ time : new Date(), method : 'GET' , path : '/process' , headers : req.headers ,
 				query : req.query , duration : (random(15000,30000)/1000).toString()+"sec"});
 })
+
+app.use(bodyParser.json()); // support json encoded bodies
+
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.post('/process', function(req, res){
+	setTimeout(function(){
+		 res.json({ time : new Date(), method : 'POST' , path : '/process' , headers : req.headers ,
+				query : req.query , body:req.body , duration : (random(15000,30000)/1000).toString()+"sec"});
+	},random(15000,30000))
+})
+
+app.put('/process', function(req, res){
+	setTimeout(function(){
+		 res.json({ time : new Date(), method : 'PUT' , path : '/process' , headers : req.headers ,
+				query : req.query , body:req.body , duration : (random(15000,30000)/1000).toString()+"sec"});
+	},random(15000,30000))
+})
+
+app.delete('/process', function(req, res){
+	setTimeout(function(){
+		 res.json({ time : new Date(), method : 'DELETE' , path : '/process' , headers : req.headers ,
+				query : req.query , body:req.body , duration : (random(15000,30000)/1000).toString()+"sec"});
+	},random(15000,30000))
+})
