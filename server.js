e0fce2948c0b6e3fc5f34ce70583be9e06394c45
HarshulDang <dangharshul199430@gmail.com>
2018-11-22 20:50:20 +0530

    Endpoint2 /stats started and found number of requests hit per sec

--- server.js
+++ server.js
@@ -43,6 +43,40 @@ app.delete('/process', function(req, res){
 
 //----------------------------------------------------------------------------------------------------
 
+//stats
+
+//----------------------------------------------------------------------------------------------------
+var requests = [];
+var requestTrimThreshold = 5000;
+var requestTrimSize = 4000;
+
+app.all("/stats", function(req, res, next) {
+	requests.push(Date.now());
+
+    // now keep requests array from growing forever
+    if (requests.length > requestTrimThreshold) {
+        requests = requests.slice(0, requests.length - requestTrimSize);
+    }
+    next();
+
+    req.start = Date.now();
+    next();
+
+    var time = Date.now() - req.start;
+    var now = Date.now();
+    var aSecAgo = now - 1000;
+    var secCnt = 0;
+    // since recent requests are at the end of the array, search the array
+    // from back to front
+    for (var i = requests.length - 1; i >= 0; i--) {
+        if (requests[i] >= aSecAgo) {
+            ++secCnt;
+        } else {
+            break;
+        }
+    }
+//----------------------------------------------------------------------------------------------------
+
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
