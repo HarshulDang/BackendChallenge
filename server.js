43417f86878a1c20cff86f858da97001b53b42ad
HarshulDang <dangharshul199430@gmail.com>
2018-11-22 20:34:14 +0530

    Wrote random function calculate the duration

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ var start = new Date();
 var bodyParser = require('body-parser');
 var responseTime = require('response-time');
 
+//process
+//---------------------------------------------------------------------------------------------------
+ function random(low, high) {
+   return Math.random() * (high - low) + low;
+}
 app.get('/process', function (req, res) {
 	
 		 res.json({ time : new Date(), method : 'GET' , path : '/process' , headers : req.headers ,
-				query : req.query});
+				query : req.query , duration : (random(15000,30000)/1000).toString()+"sec"});
 })
